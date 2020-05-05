4f49bf60e17436cd65144f2cd04b74471d2319c2
Eric Hiltz <eric.m.hiltz@gmail.com>
2020-05-05 14:33:16 -0400

    Got the basics of the navigation panel written

--- js/app.js
+++ js/app.js
@@ -18,6 +18,14 @@
  * 
 */
 
+// Then nav bar element
+const navBar = document.querySelector('.navbar__menu');
+
+// The navigation bar list
+const navBarList = navBar.querySelector('#navbar__list');
+
+//  All sections held in a node list
+const sections = document.querySelectorAll('section');
 
 /**
  * End Global Variables
@@ -25,6 +33,25 @@
  * 
 */
 
+// Function to get the bottom coordinates of a given element
+function getBottom(elem){
+    const coordinates = elem.getBoundingClientRect();
+    return coordinates.top;
+}
+
+// Translates the data attribute to it's ID
+function dataToID(txt){
+    return txt.split(' ').join('').toLowerCase();
+}
+
+// //Function to scroll to the active class
+// function scrollToElement(elem){
+//     window.scrollToElement(elem);
+//     // window.scrollTo({
+//     //     top: getBottom(elem) - window.screenY,
+//     //     behavior: 'smooth'
+//     // });
+// }
 
 
 /**
@@ -34,6 +61,54 @@
 */
 
 // build the nav
+// Function to get the navigation list elements
+function addNavs(){
+
+    const docFrag = document.createDocumentFragment(); // Document frag to add to
+
+    for(let i = 0; i < sections.length; i++){
+        const listItem = document.createElement('li');  // Create the new item
+        listItem.textContent = sections[i].dataset.nav; // Add the name of each section
+        docFrag.appendChild(listItem); // Add the item to the doc frag
+    }
+
+    navBarList.appendChild(docFrag); // Add the nav buttons to the document
+    addNavListeners(); //Add event listeners
+}
+
+// Wrapper for adding the nav click listeners
+function addNavListeners(){
+    const li = navBarList.querySelectorAll('li') // Linst nodes in the ul
+    for(let i = 0; i < li.length; i++){
+        li[i].addEventListener('click', navClick); //Add the event listener to the nav bar options
+    }
+}
+
+// Function for navigation click listener
+function navClick(event){
+
+    // Get element of what to go to.  Id is determined by the list name
+    const aClass = document.querySelector('.your-active-class'); // Active class
+    const idName = dataToID(event.target.textContent);
+    const elem = document.querySelector('#'+ idName);
+    
+    // Check if the link is to the currently active section
+    if (elem !== aClass){
+        // If link is to a new section, change the active section
+        aClass.classList.toggle('your-active-class');
+        elem.classList.toggle('your-active-class');
+    }
+    elem.scrollIntoView({
+        behavior: 'smooth'
+    });
+    // Scroll to the active section
+    // scrollToElement(elem);
+}
+
+// Function to use in the scrolling event
+function scrollListener(event){
+    console.log(event)
+}
 
 
 // Add class 'active' to section when near top of viewport
@@ -41,7 +116,13 @@
 
 // Scroll to anchor ID using scrollTO event
 
-
+// Opening function that runs on the onLoad event
+function openingFunc(){
+    //Add the navigation fields
+    addNavs();
+    //Add a scroll listener to the window
+    window.addEventListener('scroll',scrollListener)
+}
 /**
  * End Main Functions
  * Begin Events
